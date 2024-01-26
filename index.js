f5eddaa759ca4fd8bcf019f1755a179f976a5e6b
I Temply <bluerambles278@gmail.com>
2024-01-26 16:31:20 +0000

    Express App
    Signed-off-by: I Temply <bluerambles278@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// contents
+//timtamtemtil
+
+const express = require('express')
+const path = require('node:path')
+const bodyParser = require('body-parser');
+const request = require('request')
+const crypto = require('crypto')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+
+app.set('views', path.join(__dirname, 'public'))
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  console.log('Sending [GET]: Index')
+  res.render('index')
+})
+
+
+app.listen(3000, () => {
+  console.log('Dispute started on port 3000.')
+})
